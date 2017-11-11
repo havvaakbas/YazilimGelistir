55a686cab2edcc9d2eb89980d396c654f0a6ba4a
bprlab <bprlab@10.201.38.90>
2017-11-11 14:00:11 +0300

    carpma

---
+++ MavenProjeOrnekParent/Maven/src/main/java/hesaplama.java
@@ -0,0 +1,11 @@
+
+public class hesaplama {
+	public static void main(String[] args) {
+		int sonuc = topla(25, 22);
+		System.out.println(sonuc);
+	}
+	public static int topla(int sayi1, int sayi2) {
+		return sayi1+sayi2;
+	}
+
+}
